a14b201c0836cd06413df873db65a44e994667ff
stephx202 <stephhernandez202@gmail.com>
2023-12-22 10:18:45 -0800

    titles displaying on entries form

--- public/index.js
+++ public/index.js
@@ -1,3 +1,38 @@
+$(document).ready(()=>{
+
+    //call to /journal to check if getting data on console
+    $.get('/journal', (data)=>{
+        console.log(data);
+        titleOptions(data)
+    })
 
 
+})
+
+//function to display title of entries on the entries form
+const titleOptions = (titles)=>{
+    const selectTitle = $('#selectTitle');
+    selectTitle.empty();
+
+    //for each title, will be an option on entries form
+    titles.forEach((entry)=>{
+        selectTitle.append($('<option>',{
+            value: entry.title,
+            text: entry.title
+        }))
+    })
+}
+
+
+
+//function with call based on current selected Title and respond with data on that row
+const selectedTitleData = (selectedTitle)=>{
+    $.get(`/journal/${selectedTitle}`, (data)=>{
+        console.log(data);
+    });
+};
+
+//function to display data of selected title on the page
+const displayTitleData = (titleData)=>{
+}
 //need to encode title that was selected
